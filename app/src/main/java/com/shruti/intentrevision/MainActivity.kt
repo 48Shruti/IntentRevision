541394fbafdb3232a35d156387a82ff463b72706
Shruti sharma <105702669+48Shruti@users.noreply.github.com>
2025-01-15 19:00:33 +0300

    password changes

--- app/src/main/java/com/shruti/intentrevision/MainActivity.kt
+++ app/src/main/java/com/shruti/intentrevision/MainActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.widget.Toast
 import com.shruti.intentrevision.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding : ActivityMainBinding
+    var isPasswordVisible = false
+    var isConfirmVisible = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -22,9 +25,9 @@ class MainActivity : AppCompatActivity() {
                 binding.etcontact.error ="Enter contact"
             }else if(binding.etpassword.text.isNullOrEmpty()){
                 binding.etpassword.error ="Enter password"
-            }else if(binding.etconfirmpassword.text.isNullOrEmpty()){
-                binding.etconfirmpassword.error ="Enter confirm password"
-            }else if(binding.etpassword.text.toString() != binding.etconfirmpassword.text.toString()){
+            }else if(binding.etconfpassword.text.isNullOrEmpty()){
+                binding.etconfpassword.error ="Enter confirm password"
+            }else if(binding.etpassword.text.toString() != binding.etconfpassword.text.toString()){
                     Toast.makeText(this,"Passwords are not matched", Toast.LENGTH_SHORT).show()
                 }
             else{
@@ -36,6 +39,28 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this,"Passwords are  matched", Toast.LENGTH_SHORT).show()
                 }
             }
+        binding.iveyeclosedPass.setOnClickListener {
+            isPasswordVisible = !isPasswordVisible
+            if (isPasswordVisible){
+                binding.etpassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                binding.iveyeclosedPass.setImageResource(R.drawable.baseline_remove_red_eye_24)
+            }else{
+                binding.etpassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                binding.iveyeclosedPass.setImageResource(R.drawable.img)
+            }
+            binding.etpassword.setSelection(binding.etpassword.text.length)
+        }
+        binding.iveyeclosedconf.setOnClickListener {
+            isConfirmVisible = !isConfirmVisible
+            if (isConfirmVisible){
+                binding.etconfpassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                binding.iveyeclosedconf.setImageResource(R.drawable.baseline_remove_red_eye_24)
+            }else{
+                binding.etconfpassword.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                binding.iveyeclosedconf.setImageResource(R.drawable.img)
+            }
+            binding.etconfpassword.setSelection(binding.etconfpassword.text.length)
+        }
         }
     }
 
